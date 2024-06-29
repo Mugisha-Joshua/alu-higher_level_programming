3818c90e8af436be9185f24c4be735414d6a0d8c
Mugisha-Joshua <o.mugisha2@alustudent.com>
2024-06-29 00:40:41 -0700

    first

---
+++ python-if_else_loops_functions/0-positive_or_negative.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10, 10)
+if number > 0:
+    print("Number is positive")
+elif number < 0:
+    print("Number is negative")
+else:
+    print("NUMBER IS ZERO")
