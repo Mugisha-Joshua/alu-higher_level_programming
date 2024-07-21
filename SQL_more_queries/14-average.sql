122cecb834be4b37b2685aed6e887224a4bb2023
Mugisha-Joshua <o.mugisha2@alustudent.com>
2024-07-21 03:37:44 -0700

    readme +0-file

--- SQL_more_queries/14-average.sql
+++
@@ -1,2 +0,0 @@
--- script computes average score of all records in second table
-SELECT AVG(score) AS average FROM second_table;
--- SQL_more_queries/5-full_table.sql
+++
@@ -1,2 +0,0 @@
--- script that prints the full description of the table first_table from the db hbtn_0c_0 in MySQL server.
-SHOW CREATE table first_table;
